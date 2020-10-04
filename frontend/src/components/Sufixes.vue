6c2941b464f80a96ae3e297453519890d5b1e616
lanaschuster <lanaschuster1@gmail.com>
2020-10-04 09:05:38 -0300

    ref sufixes

--- frontend/src/components/Sufixes.vue
+++ frontend/src/components/Sufixes.vue
@@ -1,12 +1,14 @@
 <template>
-  <ContentTable title="Sufixos" quantity="2" :items="['qwe', 'asd']">
+  <ContentTable :title="title" :quantity="sufixes.length" :items="sufixes">
     <template v-slot:fields>
       <div class="field has-addons">
         <div class="control is-expanded">
-          <input type="text" class="input" placeholder="e aqui os sufixos.">
+          <input type="text" class="input" v-model="sufix" placeholder="e aqui os sufixos.">
         </div>
         <div class="control">
-          <button class="button is-info"><span class="icon"><i class="fa fa-plus fa-xs"></i></span></button>
+          <button class="button is-info" @click="addSufix(sufix)">
+            <span class="icon"><i class="fa fa-plus fa-xs"></i></span>
+          </button>
         </div>
       </div>
     </template>
@@ -19,6 +21,19 @@ export default {
   name: 'Sufixes',
   components: {
     ContentTable
+  },
+  data() {
+    return {
+      title: 'Sufixos',
+      sufix: '',
+      sufixes: []
+    }
+  },
+  methods: {
+    addSufix(sufix) {
+      this.sufixes.push(sufix)
+      this.sufix = ''
+    }
   }
 }
 </script>
